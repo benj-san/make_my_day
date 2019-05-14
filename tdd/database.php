91aa4738cd07774fc310f5c68c8d733c130e68aa
florian Logez <flo.log09@gmail.com>
2019-05-14 11:14:35 +0200

    make request to show hello tuesday and video

--- tdd/database.php
+++ tdd/database.php
@@ -1,9 +1,11 @@
 <?php
 
-$name='';
-$password='';
+$name='root';
+$password='123';
 $host='localhost';
 $bdd='hello';
+
+$pdo = new PDO('mysql:host='.$host.';dbname='.$bdd, $name, $password);
 
 try {    $dbh = new PDO('mysql:host='.$host.';dbname='.$bdd.';charset=utf8', $name, $password);
    } catch (PDOException $e) {
--- tdd/index.php
+++ tdd/index.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'database.php';
+$requeteSql = 'SELECT name, day FROM music WHERE day="tuesday"';
+$resultSql = $pdo->query($requeteSql);
+$info = $resultSql->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!doctype html>
@@ -10,14 +13,12 @@ require_once 'database.php';
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>We happy few</title>
-    <link href="https://fonts.googleapis.com/css?family=Anton|Modak" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Anton%7CModak" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 
-    <h1>Hello</h1>
-    <iframe width="840" height="473" src="https://www.youtube.com/embed/Z68u6dJqoI0?autoplay=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<h1>Hello <?= $info[2]['day'] ?></h1>
+<iframe width="840" height="473" src="https://www.youtube.com/embed/<?= $info[2]['name'] ?>?autoplay=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
 </html>
-
-
